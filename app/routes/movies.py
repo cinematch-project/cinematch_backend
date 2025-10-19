44a30b780e4ef090d2a57d646351b7854c3dd3a4
yura4ka <yura4ka05@gmail.com>
2025-10-19 13:22:35 +0300

    feat: search favorite movies

--- app/routes/movies.py
+++ app/routes/movies.py
@@ -1,3 +1,4 @@
+from typing import Annotated
 from fastapi import APIRouter, Query
 from sqlmodel import select, or_
 from app.dependencies.db import SessionDep
@@ -7,10 +8,10 @@ from app.models.movie import Movie, MoviePublic
 router = APIRouter()
 
 
-@router.get("/")
+@router.get("/search")
 def get_movies(
     session: SessionDep,
-    search: str | None = Query(None, description="Search term for title or keywords"),
+    search: Annotated[str, Query(description="Search term for title or keywords")],
 ) -> list[MoviePublic]:
     if not search:
         return []
@@ -29,3 +30,27 @@ def get_movies(
 
     results: list[Movie] = session.exec(stmt).all()
     return [m.to_public() for m in results]
+
+@router.get("/batch")
+def get_movies_batch(
+    session: SessionDep, 
+    ids: Annotated[list[int], Query(description="List of movie IDs")]
+) -> list[MoviePublic]:
+    """
+    Get movies by ID
+    """
+    if not ids:
+        return []
+
+    ordered_ids = list(set(ids))
+
+    stmt = select(Movie).where(Movie.id.in_(ordered_ids))
+    results: list[Movie] = session.exec(stmt).all()
+
+    by_id = {m.id: m for m in results}
+    response: list[MoviePublic] = []
+    for _id in ids:
+        movie = by_id.get(_id)
+        if movie:
+            response.append(movie.to_public())
+    return response
